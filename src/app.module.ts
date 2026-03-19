8bfe44cf767e05cd887c58a4f3b9066718f7487e
Jonny200220 <jonnyrmzhdz@gmail.com>
2026-03-19 13:31:40 -0600

    feat: instalando nestjs-config para las variables de entorno, class-transformer y class-validatos para transformar los datos recibidos en requests para asegurar un tipo, instancia o valor de un objeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GeminiModule } from './gemini/gemini.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GeminiModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
